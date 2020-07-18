895aed9e3c4e2d7d4440fb875885f47018e1e018
Lars Gaubisch <l.gaubisch@googlemail.com>
2020-07-18 11:35:23 +0200

    added source/destination path checks

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/fatih/color v1.9.0
+	github.com/rebel-l/go-utils v1.2.0-rc.3
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/fatih/color"
+
+	"github.com/rebel-l/go-utils/osutils"
 )
 
 const (
 	source      = "D:\\CarMusic - Prepare" // TODO: config file
 	destination = "F:\\"                   // TODO: config file
+)
+
+var (
+	errPathNotExisting = errors.New("path does not exist")
+
+	errFormat = color.New(color.FgRed)
 )
 
 func main() {
@@ -23,5 +32,21 @@ func main() {
 	fmt.Printf("%s %s\n", description.Sprint("Destination:"), info.Sprint(destination))
 
 	fmt.Println()
-	_, _ = title.Println("MP3 sync finished successful!")
+	if err := do(); err != nil {
+		_, _ = errFormat.Printf("MP3 sync finished with error: %v\n", err)
+	} else {
+		_, _ = title.Println("MP3 sync finished successful!")
+	}
+}
+
+func do() error {
+	if !osutils.FileOrPathExists(source) {
+		return fmt.Errorf("%w: %s", errPathNotExisting, source)
+	}
+
+	if !osutils.FileOrPathExists(destination) {
+		return fmt.Errorf("%w: %s", errPathNotExisting, destination)
+	}
+
+	return nil
 }
